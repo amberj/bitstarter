c2924849e7a8c108e4e2427dbaddfcc62cc0bfc7
vagrant <vagrant@happyly-dev-vm>
2013-07-16 00:44:05 +0000

    text in index.html is displayed now

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fbuf = fs.readFileSync('./index.html');
+  strdata = fbuf.toString();
+  response.send('strdata');
 });
 
 var port = process.env.PORT || 5000;
